5d50c3db911dd4fcdc82926a2e0017c187a363a1
Aditya Musale <adityamusale4477@gmail.com>
2023-11-18 22:21:03 +0530

    added map in contactpage

--- src/pages/contact/contact.page.jsx
+++ src/pages/contact/contact.page.jsx
@@ -15,6 +15,9 @@ const ContactInfo = ({heading, num, email}) => {
 const Contact = () => {
   return (
     <main className='layout'>
+      <div className='contact_page_map'>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.864935893867!2d73.84665057453688!3d18.53500466873446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c0841ff0f2d1%3A0xc029665f68dc9d93!2sALANKAR%20APARTMENT!5e0!3m2!1sen!2sin!4v1700325313952!5m2!1sen!2sin" width="100%" height="100%" style={{border:'0'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
       <section className='contact_page section'>
         <div className='contact_page_wrapper'>
           <div className='contact_page-heading'>
